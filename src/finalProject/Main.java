ff7fd78985d012c0938da161eb5007ce91bac855
jiminijr <jiminijr000106@gmail.com>
2024-06-05 21:42:28 +0900

    Fix Main fuction for starting app

--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -1,5 +1,18 @@
 package finalProject;
 
-public class Main {
+import java.awt.EventQueue;
 
+public class Main {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ScreenFrame frame = new ScreenFrame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
